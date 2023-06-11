1d2c8022daaface71cf4d722e9b2d21e2b462fbf
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2023-06-12 00:46:10 +0500

    some changes

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const API_KEY =
-  "pcSJxqNPg9q0QJP5-tX81Io5YY_XFBhjdEUK6Vd3gXKIiOCnXbp4ZSzXiEAagCvg";
-
-const URL =
-  "https://api.qr-code-generator.com/v1/create?access-token=pcSJxqNPg9q0QJP5-tX81Io5YY_XFBhjdEUK6Vd3gXKIiOCnXbp4ZSzXiEAagCvg";
-
 // const response = fetch("/test.txt")
 //   .then((res) => res.json())
 //   .then((data) => {
@@ -13,38 +7,41 @@ const URL =
 //     console.log(err);
 //   });
 
-const data = {
-  frame_name: "no-frame",
-  qr_code_text: "https://www.qr-code-generator.com/",
-  image_format: "SVG",
-  background_color: "#ffffff",
-  foreground_color: "#fa6e79",
-  marker_right_inner_color: "#2d7cda",
-  marker_right_outer_color: "#00bfff",
-  marker_left_template: "version13",
-  marker_right_template: "version13",
-  marker_bottom_template: "version13",
-};
+const apiUrl = "https://api.qrserver.com/v1/create-qr-code/";
 
-const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-};
+const data = prompt("Shifrlamoqchi bo'lgan matningizni kiriting:");
 
-const Urgench = fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Urgench&appid=28824b46851a478eaa59e646200fd6cd"
-).then((res) => res.json());
+fetch(
+  `${apiUrl}?size=200x200&color=00FF00&bgcolor=000&data=${encodeURIComponent(
+    data
+  )}`
+)
+  .then((res) => res.blob())
+  .then((blob) => {
+    const qrCodeImage = document.createElement("img");
+    qrCodeImage.src = URL.createObjectURL(blob);
+    qrCodeImage.alt = "QR Code";
 
-const Tashkent = fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=28824b46851a478eaa59e646200fd6cd"
-).then((res) => res.json());
+    const qrcodeContainer = document.getElementById("qrcode");
+    qrcodeContainer.innerHTML = "";
 
-const Samarkand = fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Samarkand&appid=28824b46851a478eaa59e646200fd6cd"
-).then((res) => res.json());
+    qrcodeContainer.appendChild(qrCodeImage);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// const Urgench = fetch(
+//   "https://api.openweathermap.org/data/2.5/weather?q=Urgench&appid=28824b46851a478eaa59e646200fd6cd"
+// ).then((res) => res.json());
+
+// const Tashkent = fetch(
+//   "https://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=28824b46851a478eaa59e646200fd6cd"
+// ).then((res) => res.json());
+
+// const Samarkand = fetch(
+//   "https://api.openweathermap.org/data/2.5/weather?q=Samarkand&appid=28824b46851a478eaa59e646200fd6cd"
+// ).then((res) => res.json());
 
 // Promise.all([Urgench, Tashkent, Samarkand])
 //   .then((res) => {
